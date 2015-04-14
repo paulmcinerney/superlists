c68805913e034a864eacb6090e5f8e1700930b95
Paul Mcinerney <paulmcinerney@Pauls-MacBook-Pro-Retina.local>
2015-04-14 13:51:09 +0100

    now adjusted the views and tests to handle the foreign key of List object

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        #new_item_text = request.POST['item_text']
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    return render(request,'lists/home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request,'lists/home.html',{'items': items})
+    return render(request, 'lists/list.html', {'items': items})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
