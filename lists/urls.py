44b3e1fa95d569e7cddf0acda913e64e9f79c260
Paul Mcinerney <paulmcinerney@Pauls-MacBook-Pro-Retina.local>
2015-04-15 13:41:54 +0100

    new URL + view for adding to existing lists. FT passes :-)

--- lists/urls.py
+++ lists/urls.py
@@ -5,9 +5,8 @@ from lists import views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-    # url(r'^lists/(\d+)/$', views.view_list,name='view_list'),
-    # url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-    # url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', views.view_list,name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
     #url(r'^admin/', include(admin.site.urls)),
 )
